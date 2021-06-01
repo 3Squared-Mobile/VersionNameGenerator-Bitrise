d58afec1d6c1778959cc3b3118c2b440692298be
Sam Oakley <sam.oakley@3squared.com>
2021-06-01 14:17:35 +0100

    Add "wip" labelled versions as extra output

--- step.sh
+++ step.sh
@@ -10,14 +10,19 @@ GITBRANCH=`git rev-parse --abbrev-ref HEAD`
 # Store the bash field seperator to be reset later. We use this to split strings.
 OLDIFS=$IFS
 
+GENERATED_VERSION_NAME=""
+GENERATED_VERSION_NAME_DETAILED=""
+
 # If we're on main, the commit should be tagged, so a simple git describe will do.
 if [[ "$GITBRANCH" = "main" || "$GITBRANCH" = "master" ]]; then
     DESCRIBE=`git describe`
     # describe can fail if there are no tags.
     if [ "$DESCRIBE" == "" ]; then
         GENERATED_VERSION_NAME="0.0.0"
+        GENERATED_VERSION_NAME_DETAILED="0.0.0"
     else
         GENERATED_VERSION_NAME=$DESCRIBE
+        GENERATED_VERSION_NAME_DETAILED=$GENERATED_VERSION_NAME
     fi
 else
     # If the branch name matches the git flow standard for releases or hotfixes, we can use the 2nd part of the branch name as the version.
@@ -28,12 +33,14 @@ else
         TYPE="${SPLIT[0]}"
         VERSION="${SPLIT[1]}"
         GENERATED_VERSION_NAME=$VERSION
+        GENERATED_VERSION_NAME_DETAILED=$GENERATED_VERSION_NAME
     else 
         # If we're building any other branch, we compute a version number based on the previous one.
         PREVIOUS=`git tag -l '[0-9].*' --sort version:refname | tail -n1`
         if [ "$PREVIOUS" = "" ]; then
             # No tags!
             GENERATED_VERSION_NAME="0.0.0"
+            GENERATED_VERSION_NAME_DETAILED="$GENERATED_VERSION_NAME-wip"
         else 
             # Split the version number into 3 parts, increment minor.
             IFS='.'
@@ -42,6 +49,7 @@ else
             MINOR="${SPLIT[1]}"
             PATCH="${SPLIT[2]}"
             GENERATED_VERSION_NAME="$MAJOR.$((MINOR + 1)).0"
+            GENERATED_VERSION_NAME_DETAILED="$GENERATED_VERSION_NAME-wip"
         fi
     fi
 fi
@@ -49,5 +57,6 @@ fi
 IFS=$OLDIFS
 
 envman add --key GENERATED_VERSION_NAME --value "${GENERATED_VERSION_NAME}"
+envman add --key GENERATED_VERSION_NAME_DETAILED --value "${GENERATED_VERSION_NAME_DETAILED}"
 
 exit 0
