923729c93a0170206f3f20f4530efb43528cd618
Sam Oakley <sam.oakley@3squared.com>
2021-07-19 10:53:36 +0100

    Update version generation to that tag always takes precedence
    This allows previous tags to be built, and removes requirement that main needed to be built for a tagged release.

--- step.sh
+++ step.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+# set -x
 
 # Make sure we have all tags. These aren't pulled down by default.
 git fetch --tags
@@ -13,18 +13,13 @@ OLDIFS=$IFS
 GENERATED_VERSION_NAME=""
 GENERATED_VERSION_NAME_DETAILED=""
 
-# If we're on main, the commit should be tagged, so a simple git describe will do.
-if [[ "$GITBRANCH" = "main" || "$GITBRANCH" = "master" ]]; then
-    DESCRIBE=`git describe`
-    # describe can fail if there are no tags.
-    if [ "$DESCRIBE" == "" ]; then
-        GENERATED_VERSION_NAME="0.0.0"
-        GENERATED_VERSION_NAME_DETAILED="0.0.0"
-    else
-        GENERATED_VERSION_NAME=$DESCRIBE
-        GENERATED_VERSION_NAME_DETAILED=$GENERATED_VERSION_NAME
-    fi
+TAG=`git describe --exact-match`
+if [ "$TAG" != "" ]; then
+    echo "Tag is $TAG, using as version."
+    GENERATED_VERSION_NAME=$TAG
+    GENERATED_VERSION_NAME_DETAILED=$TAG
 else
+    echo "Commit is not tagged, generating a version number."
     # If the branch name matches the git flow standard for releases or hotfixes, we can use the 2nd part of the branch name as the version.
     if [[ $GITBRANCH == release* || $GITBRANCH == hotfix* ]]; then
         # Setting the field seperator to / allows us to split the branch name.
@@ -32,13 +27,19 @@ else
         read -a SPLIT <<< "$GITBRANCH"
         TYPE="${SPLIT[0]}"
         VERSION="${SPLIT[1]}"
+
+        echo "Building a $TYPE branch, version will be $VERSION."
+
         GENERATED_VERSION_NAME=$VERSION
         GENERATED_VERSION_NAME_DETAILED=$GENERATED_VERSION_NAME
     else 
         # If we're building any other branch, we compute a version number based on the previous one.
+        echo "Branch is not a release or hotfix ($GITBRANCH)."
+
         PREVIOUS=`git tag -l '[0-9].*' --sort version:refname | tail -n1`
         if [ "$PREVIOUS" = "" ]; then
             # No tags!
+            echo "No previous tags to base version on, version will be 0.0.0."
             GENERATED_VERSION_NAME="0.0.0"
             GENERATED_VERSION_NAME_DETAILED="$GENERATED_VERSION_NAME-wip"
         else 
@@ -50,6 +51,8 @@ else
             PATCH="${SPLIT[2]}"
             GENERATED_VERSION_NAME="$MAJOR.$((MINOR + 1)).0"
             GENERATED_VERSION_NAME_DETAILED="$GENERATED_VERSION_NAME-wip"
+
+            echo "Previous tag is $PREVIOUS, version will increment minor value."
         fi
     fi
 fi
@@ -58,5 +61,8 @@ IFS=$OLDIFS
 
 envman add --key GENERATED_VERSION_NAME --value "${GENERATED_VERSION_NAME}"
 envman add --key GENERATED_VERSION_NAME_DETAILED --value "${GENERATED_VERSION_NAME_DETAILED}"
+
+echo "GENERATED_VERSION_NAME = $GENERATED_VERSION_NAME"
+echo "GENERATED_VERSION_NAME_DETAILED = $GENERATED_VERSION_NAME_DETAILED"
 
 exit 0
