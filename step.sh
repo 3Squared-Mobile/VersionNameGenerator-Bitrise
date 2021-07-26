d4745ea335ae46af8b33ee44d55cc13356deefef
Sam Oakley <sam.oakley@3squared.com>
2021-07-26 13:34:22 +0100

    Use Bitrise env vars if available

--- step.sh
+++ step.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 # set -x
 
-# Make sure we have all tags. These aren't pulled down by default.
-git fetch --tags
-
-# Get current branch name.
-GITBRANCH=`git rev-parse --abbrev-ref HEAD`
-
 # Store the bash field seperator to be reset later. We use this to split strings.
 OLDIFS=$IFS
+
+# Get current branch name.
+# If Bitrise env var (BITRISE_GIT_BRANCH) is not passed in, use git command.
+if [[ -z "$BITRISE_GIT_BRANCH" ]]; then
+    BRANCH=`git rev-parse --abbrev-ref HEAD`
+    echo "BITRISE_GIT_BRANCH is undefined, using '$BRANCH'."
+else
+    BRANCH=$BITRISE_GIT_BRANCH
+    echo "BITRISE_GIT_BRANCH is '$BRANCH'."
+fi
+
+# Get current tag.
+# If Bitrise env var (BITRISE_GIT_TAG) is not passed in, use git command.
+if [[ -z "$BITRISE_GIT_TAG" ]]; then
+    # Make sure we have all tags. These aren't pulled down by default.
+    git fetch --tags
+    TAG=`git describe --exact-match`
+    echo "BITRISE_GIT_TAG is undefined, using '$TAG'."
+else
+    TAG=$BITRISE_GIT_TAG
+    echo "BITRISE_GIT_TAG is '$TAG'."
+fi
 
 GENERATED_VERSION_NAME=""
 GENERATED_VERSION_NAME_DETAILED=""
 
-TAG=`git describe --exact-match`
 if [ "$TAG" != "" ]; then
     echo "Tag is $TAG, using as version."
     GENERATED_VERSION_NAME=$TAG
@@ -21,10 +36,10 @@ if [ "$TAG" != "" ]; then
 else
     echo "Commit is not tagged, generating a version number."
     # If the branch name matches the git flow standard for releases or hotfixes, we can use the 2nd part of the branch name as the version.
-    if [[ $GITBRANCH == release* || $GITBRANCH == hotfix* ]]; then
+    if [[ $BRANCH == release* || $BRANCH == hotfix* ]]; then
         # Setting the field seperator to / allows us to split the branch name.
         IFS='/'
-        read -a SPLIT <<< "$GITBRANCH"
+        read -a SPLIT <<< "$BRANCH"
         TYPE="${SPLIT[0]}"
         VERSION="${SPLIT[1]}"
 
@@ -34,7 +49,7 @@ else
         GENERATED_VERSION_NAME_DETAILED=$GENERATED_VERSION_NAME
     else 
         # If we're building any other branch, we compute a version number based on the previous one.
-        echo "Branch is not a release or hotfix ($GITBRANCH)."
+        echo "Branch is not a release or hotfix ($BRANCH)."
 
         PREVIOUS=`git tag -l '[0-9].*' --sort version:refname | tail -n1`
         if [ "$PREVIOUS" = "" ]; then
